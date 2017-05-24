0dd53f61b932129c05425c3f0df701937083a36a
Jo Dickson <jogreybill@gmail.com>
2017-05-24 10:05:49 -0500

    Updated shortcode interface layout

--- includes/wp-scif-interface.php
+++ includes/wp-scif-interface.php
@@ -31,16 +31,20 @@ $shortcodes = WP_SCIF_Config::installed_shortcodes();
                 </div>
             </div>
         </div>
-        <div id="shortcode-editors">
-            <?php
-            foreach( $shortcodes as $shortcode ) {
-                echo $shortcode->get_form_markup();
-            }
-            ?>
-        </div>
-		<div id="scif-preview-window">
-			<iframe id="scif-preview-iframe" src="<?php echo admin_url( 'admin-post.php?action=render_preview' ); ?>"></iframe>
+		<div class="cols">
+			<div class="col-left" id="shortcode-editors">
+				<?php
+				foreach( $shortcodes as $shortcode ) {
+					echo $shortcode->get_form_markup();
+				}
+				?>
+			</div>
+			<div class="col-right" id="scif-preview-window">
+				<strong class="field-label">Preview</strong>
+				<img id="scif-loading" src="<?php echo admin_url( '/wp-admin/images/spinner-2x.gif' ); ?>" alt="Loading..." style="display: none;">
+				<iframe id="scif-preview-iframe" src="<?php echo admin_url( 'admin-post.php?action=render_preview' ); ?>"></iframe>
+			</div>
 		</div>
-        <button class="button-primary" id="wp-scif-submit">Insert into Post</button>
+		<button class="button-primary" id="wp-scif-submit">Insert into Post</button>
     </div>
 </div>
